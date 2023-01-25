236f86ee04ff8de9905534370216c78347855f9c
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-25 07:52:59 +0700

    Close Connection (is a must)

--- Connection.php
+++ Connection.php
@@ -10,6 +10,8 @@ $password = "";
 try {
     $con = new PDO("mysql:host=$host:$port;dbname=$dbname", $username, $password);
     echo "Sukses terhubung dengan database";
+    // Close Connection
+    $con = null;
 } catch (PDOException $exception) {
     echo "Gagal terhubung dengan database, error : {$exception->getMessage()}";
 }
