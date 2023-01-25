c530e96612986247777f6258ccde7f025a23e19e
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-25 08:21:44 +0700

    Catch Error with exception

--- ExecuteSQL.php
+++ ExecuteSQL.php
@@ -2,13 +2,15 @@
 
 require_once __DIR__ . "/ConDB.php";
 
-$con = conDB();
-
-$sql = <<<sql
+try {
+    $con = conDB();
+    $sql = <<<sql
     INSERT INTO customer(id,name,email) 
     VALUES ("1","Rizal", "rizal@gmail.com");
-sql;
+    sql;
 
-$con->exec($sql);
-
-$con = null;
+    $con->exec($sql);
+    $con = null;
+} catch (PDOException $exception) {
+    echo "Proses Gagal : " . $exception->getMessage() . PHP_EOL;
+}
